6e44f8408e5a0a97620ef6f8033cca27781d5ed4
戴峰 <daifeng@MacBook-Pro.local>
2020-11-08 22:58:48 +0800

    init commit

---
+++ src/main/java/Sss/Rrr/RrrApplication.java
@@ -0,0 +1,16 @@
+package Sss.Rrr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author daifeng
+ */
+@SpringBootApplication
+public class RrrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sss/Rrr/RrrApplicationTests.java
@@ -0,0 +1,13 @@
+package Sss.Rrr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
